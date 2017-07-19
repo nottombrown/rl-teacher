e4fa443bfa5310d9d47af3b6f0d2ab2c6f4522b7
Tom Brown <nottombrown@gmail.com>
2017-07-19 14:38:31 -0700

    Don't double set start method

--- parallel-trpo/parallel_trpo/train.py
+++ parallel-trpo/parallel_trpo/train.py
@@ -1,3 +1,5 @@
+import os
+
 import multiprocess
 from time import clock as time
 
@@ -40,7 +42,9 @@ def train_parallel(
     # Tensorflow is not fork-safe, so we must use spawn instead
     # https://github.com/tensorflow/tensorflow/issues/5448#issuecomment-258934405
     # We use multiprocess rather than multiprocessing because Keras sets a multiprocessing context
-    multiprocess.set_start_method('spawn')
+    if not os.environ.get("SET_PARALLEL_TRPO_START_METHOD"): # Use an env variable to prevent double-setting
+        multiprocess.set_start_method('spawn')
+        os.environ['SET_PARALLEL_TRPO_START_METHOD'] = 1
 
     run_indefinitely = (runtime <= 0)
 
