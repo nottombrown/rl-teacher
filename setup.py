c755c888235a0e3e1ee4b0be26fa63b155e2621c
Tom Brown <nottombrown@gmail.com>
2017-07-26 10:37:13 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
+import sys
+
 from setuptools import setup
+
+if sys.version_info.major != 3:
+    print("This module is only compatible with Python 3, but you are running "
+          "Python {}. The installation will likely fail.".format(sys.version_info.major))
 
 setup(name='rl_teacher',
     version='0.0.1',
