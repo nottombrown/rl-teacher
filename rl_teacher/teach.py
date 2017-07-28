8d9821510fe83bdae2e5d11f39d915d71f81ee3e
Max Harms <raelifin@gmail.com>
2017-07-27 21:09:09 -0400

    Problem wasn't data with zeros, it was data with zero variance

--- rl_teacher/teach.py
+++ rl_teacher/teach.py
@@ -152,15 +152,18 @@ class ComparisonRewardPredictor():
 
         # Calculate correlation between true and predicted reward by running validation on recent episodes
         recent_paths = self.agent_logger.get_recent_paths_with_padding()
-        if recent_paths and self.agent_logger.summary_step % 10 == 0:  # Run validation every 10 iters
+        if len(recent_paths) > 1 and self.agent_logger.summary_step % 10 == 0:  # Run validation every 10 iters
             validation_q_states = np.asarray([create_segment_q_states(path) for path in recent_paths])
             q_state_reward_pred = self.sess.run(self.q_state_reward_pred, feed_dict={
                 self.segment_placeholder: validation_q_states,
                 K.learning_phase(): False
             })
-            ep_reward_pred = np.sum(q_state_reward_pred, axis=1) + 1e-12  # Avoid dividing by zero
+            ep_reward_pred = np.sum(q_state_reward_pred, axis=1)
             q_state_reward_true = np.asarray([path['original_rewards'] for path in recent_paths])
-            ep_reward_true = np.sum(q_state_reward_true, axis=1) + 1e-12  # Avoid dividing by zero
+            ep_reward_true = np.sum(q_state_reward_true, axis=1)
+            # Try to prevent np.corrcoef from blowing up on data with 0 variance
+            ep_reward_pred[0] += 1e-12
+            ep_reward_true[0] += 1e-12
             self.agent_logger.log_simple("predictor/correlations", np.corrcoef(ep_reward_true, ep_reward_pred)[0, 1])
 
         self.agent_logger.log_simple("labels/desired_labels", self.label_schedule.n_desired_labels)
