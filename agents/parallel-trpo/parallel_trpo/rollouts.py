Fix off by one error in parallel_trpo rollouts

agents/parallel-trpo/parallel_trpo/rollouts.py
@@ -84,7 +84,7 @@ class Actor(multiprocess.Process):
     def rollout(self):
         obs, actions, rewards, avg_action_dists, logstd_action_dists, human_obs = [], [], [], [], [], []
         ob = filter_ob(self.env.reset())
-        for i in range(self.max_timesteps_per_episode - 1):
+        for i in range(self.max_timesteps_per_episode):
             action, avg_action_dist, logstd_action_dist = self.act(ob)
 
             obs.append(ob)
@@ -98,7 +98,7 @@ class Actor(multiprocess.Process):
             rewards.append(rew)
             human_obs.append(info.get("human_obs"))
 
-            if done or i == self.max_timesteps_per_episode - 2:
+            if done or i == self.max_timesteps_per_episode - 1:
                 path = {
                     "obs": np.concatenate(np.expand_dims(obs, 0)),
                     "avg_action_dist": np.concatenate(avg_action_dists),