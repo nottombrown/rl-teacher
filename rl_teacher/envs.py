7b805b8a46fdcfc0b4108a42d04dec3589974fa0
Max Harms <raelifin@gmail.com>
2017-08-04 18:02:22 -0400

    Add minimal support for Atari and avoid forcing the user to have MuJoCo

--- rl_teacher/envs.py
+++ rl_teacher/envs.py
@@ -2,7 +2,7 @@ from copy import copy
 
 import gym
 import numpy as np
-from gym.envs import mujoco
+import scipy.misc as misc
 from gym.wrappers.time_limit import TimeLimit
 
 class TransparentWrapper(gym.Wrapper):
@@ -45,6 +45,49 @@ class MjViewer(TransparentWrapper):
         info["human_obs"] = human_obs
         return ob, reward, done, info
 
+class PixelEnvViewer(TransparentWrapper):
+    """Saves the raw human_obs to info that allows rendering videos subsequently"""
+
+    def __init__(self, env, flip=False, fps=40):
+        self.fps = fps
+        self.flip = flip
+        super().__init__(env)
+
+    def render_full_obs(self, full_obs):
+        return full_obs  # Don't need to do anything because we already have the source pixels!
+
+    def _step(self, a):
+        ob, reward, done, info = self.env._step(a)
+        if self.flip:
+            info["human_obs"] = np.flip(ob, axis=0)
+        else:
+            info["human_obs"] = ob
+        return ob, reward, done, info
+
+class CompressedPixelViewer(TransparentWrapper):
+    def __init__(self, env):
+        super().__init__(env)
+        self.observation_space = np.eye(84)  # HACK: This is just so the .shape == (84,84)
+
+    def _rgb2gray(self, rgb):
+        return np.dot(rgb[..., :3], [0.299, 0.587, 0.114])
+
+    def _preprocess(self, image):
+        image = self._rgb2gray(image)
+        image = misc.imresize(image, [84, 84], 'bilinear')
+        image = image.astype(np.float32) / 128.0 - 1.0
+        return image
+
+    def _reset(self):
+        ob = self.env.reset()
+        ob = self._preprocess(ob)
+        return ob
+
+    def _step(self, a):
+        ob, reward, done, info = self.env._step(a)
+        ob = self._preprocess(ob)
+        return ob, reward, done, info
+
 class UseReward(TransparentWrapper):
     """Use a reward other than the normal one for an environment.
      We do this because humans cannot see torque penalties
@@ -78,7 +121,7 @@ class TimeLimitTransparent(TimeLimit, TransparentWrapper):
 def limit(env, t):
     return TimeLimitTransparent(env, max_episode_steps=t)
 
-def task_by_name(name, short=False):
+def task_by_name(name, original_name=None, short=False):
     if name == "reacher":
         return reacher(short=short)
     elif name == "humanoid":
@@ -98,17 +141,27 @@ def task_by_name(name, short=False):
     elif name in ["doublependulum"]:
         return double_pendulum()
     else:
-        raise ValueError(name)
+        try:
+            # If an original_name is provided, try to make an environment from that.
+            # See "make_with_torque_removed"
+            env = gym.make(original_name)
+            env = CompressedPixelViewer(env)
+            env = PixelEnvViewer(env, flip=True, fps=30)
+            env = limit(t=500, env=env)
+            return env
+        except Exception:
+            raise ValueError(name)
 
 def make_with_torque_removed(env_id):
+    original_name = env_id
+    short = False
     if '-v' in env_id:
         env_id = env_id[:env_id.index('-v')].lower()
     if env_id.startswith('short'):
         env_id = env_id[len('short'):]
         short = True
-    else:
-        short = False
-    return task_by_name(env_id, short)  # Use our task_by_name function to get the env
+    # Use our task_by_name function to get the env
+    return task_by_name(env_id, original_name, short)
 
 def get_timesteps_per_episode(env):
     if hasattr(env, "_max_episode_steps"):
@@ -119,21 +172,15 @@ def get_timesteps_per_episode(env):
         return get_timesteps_per_episode(env.env)
     return None
 
-def simple_reacher():
-    return limit(SimpleReacher(), 50)
-
-class SimpleReacher(mujoco.ReacherEnv):
-    def _step(self, a):
-        ob, _, done, info = super()._step(a)
-        return ob, info["reward_dist"], done, info
-
 def reacher(short=False):
+    from gym.envs import mujoco  # Import here to avoid forcing the user to have MuJoCo to run Teacher
     env = mujoco.ReacherEnv()
     env = UseReward(env, reward_info_key="reward_dist")
     env = MjViewer(fps=10, env=env)
     return limit(t=20 if short else 50, env=env)
 
 def hopper(short=False):
+    from gym.envs import mujoco  # Import here to avoid forcing the user to have MuJoCo to run Teacher
     bonus = lambda a, data: (data.qpos[1, 0] - 1) + 1e-3 * np.square(a).sum()
     env = mujoco.HopperEnv()
     env = MjViewer(fps=40, env=env)
@@ -142,6 +189,7 @@ def hopper(short=False):
     return env
 
 def humanoid(standup=True, short=False):
+    from gym.envs import mujoco  # Import here to avoid forcing the user to have MuJoCo to run Teacher
     env = mujoco.HumanoidEnv()
     env = MjViewer(env, fps=40)
     env = UseReward(env, reward_info_key="reward_linvel")
@@ -151,6 +199,7 @@ def humanoid(standup=True, short=False):
     return limit(env, 300 if short else 1000)
 
 def double_pendulum():
+    from gym.envs import mujoco  # Import here to avoid forcing the user to have MuJoCo to run Teacher
     bonus = lambda a, data: 10 * (data.site_xpos[0][2] - 1)
     env = mujoco.InvertedDoublePendulumEnv()
     env = MjViewer(env, fps=10)
@@ -159,6 +208,8 @@ def double_pendulum():
     return env
 
 def pendulum():
+    from gym.envs import mujoco  # Import here to avoid forcing the user to have MuJoCo to run Teacher
+
     # bonus = lambda a, data: np.concatenate([data.qpos, data.qvel]).ravel()[1] - 1.2
     def bonus(a, data):
         angle = data.qpos[1, 0]
@@ -171,6 +222,7 @@ def pendulum():
     return env
 
 def cheetah(short=False):
+    from gym.envs import mujoco  # Import here to avoid forcing the user to have MuJoCo to run Teacher
     env = mujoco.HalfCheetahEnv()
     env = UseReward(env, reward_info_key="reward_run")
     env = MjViewer(env, fps=20)
@@ -178,6 +230,7 @@ def cheetah(short=False):
     return env
 
 def swimmer(short=False):
+    from gym.envs import mujoco  # Import here to avoid forcing the user to have MuJoCo to run Teacher
     env = mujoco.SwimmerEnv()
     env = UseReward(env, reward_info_key="reward_fwd")
     env = MjViewer(env, fps=40)
@@ -185,6 +238,7 @@ def swimmer(short=False):
     return env
 
 def ant(standup=True, short=False):
+    from gym.envs import mujoco  # Import here to avoid forcing the user to have MuJoCo to run Teacher
     env = mujoco.AntEnv()
     env = UseReward(env, reward_info_key="reward_forward")
     env = MjViewer(env, fps=20)
@@ -195,6 +249,7 @@ def ant(standup=True, short=False):
     return env
 
 def walker(short=False):
+    from gym.envs import mujoco  # Import here to avoid forcing the user to have MuJoCo to run Teacher
     bonus = lambda a, data: data.qpos[1, 0] - 2.0 + 1e-3 * np.square(a).sum()
     env = mujoco.Walker2dEnv()
     env = MjViewer(env, fps=30)
