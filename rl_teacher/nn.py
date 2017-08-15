18670cef5edd93e8b9dcd24237609e76a75d6eae
Max Harms <raelifin@gmail.com>
2017-08-15 14:28:46 -0700

    No more Q states (#30)
    * No more Q-states. Support for more complex environments.
    
    * Whoops I suck at git
    
    * Fix typo, remove unused discrete actionspace support

--- rl_teacher/nn.py
+++ rl_teacher/nn.py
@@ -1,10 +1,15 @@
+import numpy as np
+import tensorflow as tf
+
 from keras.layers import Dense, Dropout, LeakyReLU
 from keras.models import Sequential
 
 class FullyConnectedMLP(object):
     """Vanilla two hidden layer multi-layer perceptron"""
 
-    def __init__(self, input_dim, h_size=64):
+    def __init__(self, obs_shape, act_shape, h_size=64):
+        input_dim = np.prod(obs_shape) + np.prod(act_shape)
+
         self.model = Sequential()
         self.model.add(Dense(h_size, input_dim=input_dim))
         self.model.add(LeakyReLU())
@@ -16,5 +21,7 @@ class FullyConnectedMLP(object):
         self.model.add(Dropout(0.5))
         self.model.add(Dense(1))
 
-    def run(self, x):
+    def run(self, obs, act):
+        flat_obs = tf.contrib.layers.flatten(obs)
+        x = tf.concat([flat_obs, act], axis=1)
         return self.model(x)
