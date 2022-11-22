60e6fd8bbfb7473050c1fc7034a7ca5a0fdcb420
Jaekyung-Cho <jackyoung96@snu.ac.kr>
2022-11-22 23:20:36 +0900

    pip3 issue

--- human-feedback-api/setup.py
+++ human-feedback-api/setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(name='human_feedback_api',
     version='0.0.1',
+    py_modules=['human_feedback_api'],
     install_requires=[
         # 'Django',
         # 'dj_database_url',
