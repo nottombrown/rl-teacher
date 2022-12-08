3e443ccc1a194dca0715b2aaa08a6f9ec8f9e69d
Jaekyung-Cho <jackyoung96@snu.ac.kr>
2022-12-08 20:49:18 +0900

    why can't we use rgb_array instead of human

--- rl_teacher/envs.py
+++ rl_teacher/envs.py
@@ -33,6 +33,8 @@ class MjViewer(TransparentWrapper):
     def render_full_obs(self, full_obs):
         old_obs = self._get_full_obs()
         self._set_full_obs(full_obs)
+        # Why can't we use rgb_array
+        result = self._get_viewer("rgb_array").render()
         self._get_viewer("human").render()
         data = self._get_viewer("human")._read_pixels_as_in_window()
         result = data
