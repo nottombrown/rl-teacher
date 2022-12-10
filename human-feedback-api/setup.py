dff7b90280a3084650036673db48b7a5155bd538
Jaekyung-Cho <jackyoung96@snu.ac.kr>
2022-12-10 13:00:29 +0900

    django dependency >= 4.0.0

--- human-feedback-api/setup.py
+++ human-feedback-api/setup.py
@@ -4,7 +4,7 @@ setup(name='human_feedback_api',
     version='0.0.1',
     py_modules=['human_feedback_api'],
     install_requires=[
-        'Django ~=3.2.16',
+        'Django >=4.0.0',
         'dj_database_url ~=1.0.0',
         'gunicorn',
         'whitenoise ~=6.2.0', # version check
